7c03666b1323600ca33fb690cf5b626481e08241
Fabián Mauricio Castrillón Franco <fcastril@gmail.com>
2024-05-20 10:53:37 -0500

    BaseController

---
+++ Sana.Backend/Sana.Backend.Api/Controllers/Base/BaseController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sana.Backend.Domain.Common;
+using Sana.Backend.Domain.Port.Base;
+
+namespace Sana.Backend.Api.Controllers.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract partial class BaseController<ENT> : Controller
+        where ENT : BaseEntity, new()
+    {
+        private readonly IRepositoryBase<ENT> _repository;
+
+        protected BaseController(IRepositoryBase<ENT> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ENT ent)
+        {
+            try
+            {
+                return Ok(await _repository.Create(ent));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ENT ent)
+        {
+            try
+            {
+                return Ok(await _repository.Update(ent));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _repository.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                return Ok(await _repository.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                return Ok(await _repository.Delete(id));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+
+        }
+
+        [HttpPost("paginator")]
+        public async Task<IActionResult> Paginator(Paginate<ENT> paginate)
+        {
+            try
+            {
+                return Ok(await _repository.Paginate(paginate));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        [HttpPost("paginator/pageNo/{pageNo}/pages/{pages}")]
+        public async Task<IActionResult> PaginatorPage(int page, int lenght)
+        {
+            try
+            {
+                return Ok(await _repository.Paginate(page, lenght));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Sana.Backend/Sana.Backend.Api/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sana.Backend.Api.Controllers.Base;
+using Sana.Backend.Domain.Entities;
+using Sana.Backend.Domain.Port;
+
+namespace Sana.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : BaseController<Category>
+    {
+        public CategoryController(ICategoryRepository repository) : base(repository)
+        {
+        }
+    }
+}
